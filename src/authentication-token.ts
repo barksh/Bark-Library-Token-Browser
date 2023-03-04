962f73aca3d41a166f5ce574020719b821758e6a
wmxpy <wm@mengw.io>
2023-03-03 22:10:42 -0600

    release 1.1.0

--- src/authentication-token.ts
+++ src/authentication-token.ts
@@ -11,15 +11,37 @@ import { panic } from "./error/panic";
 
 export class BarkAuthenticationToken {
 
-    public static fromString(token: string): BarkAuthenticationToken {
+    public static fromTokenOrNull(token: string): BarkAuthenticationToken | null {
 
         const parsedToken: JWTAuthenticationToken | null = JWTToken.fromTokenOrNull(token);
 
-        if (!parsedToken) {
+        if (parsedToken === null) {
+            return null;
+        }
+        return new BarkAuthenticationToken(token, parsedToken);
+    }
+
+    public static fromTokenOrUndefined(token: string): BarkAuthenticationToken | undefined {
+
+        const authenticationToken: BarkAuthenticationToken | null = BarkAuthenticationToken.fromTokenOrNull(token);
+
+        if (authenticationToken === null) {
+            return undefined;
+        }
+        return authenticationToken;
+    }
+
+    public static fromTokenOrThrow(token: string, error?: Error): BarkAuthenticationToken {
+
+        const authenticationToken: BarkAuthenticationToken | null = BarkAuthenticationToken.fromTokenOrNull(token);
+
+        if (authenticationToken === null) {
+            if (error) {
+                throw error;
+            }
             throw panic.code(ERROR_CODE.INVALID_AUTHENTICATION_TOKEN_1, token);
         }
-
-        return new BarkAuthenticationToken(token, parsedToken);
+        return authenticationToken;
     }
 
     private readonly _rawToken: string;
--- src/refresh-token.ts
+++ src/refresh-token.ts
@@ -11,15 +11,37 @@ import { panic } from "./error/panic";
 
 export class BarkRefreshToken {
 
-    public static fromString(token: string): BarkRefreshToken {
+    public static fromTokenOrNull(token: string): BarkRefreshToken | null {
 
         const parsedToken: JWTRefreshToken | null = JWTToken.fromTokenOrNull(token);
 
-        if (!parsedToken) {
-            throw panic.code(ERROR_CODE.INVALID_REFRESH_TOKEN_1, token);
+        if (parsedToken === null) {
+            return null;
         }
-
         return new BarkRefreshToken(token, parsedToken);
+    }
+
+    public static fromTokenOrUndefined(token: string): BarkRefreshToken | undefined {
+
+        const refreshToken: BarkRefreshToken | null = BarkRefreshToken.fromTokenOrNull(token);
+
+        if (refreshToken === null) {
+            return undefined;
+        }
+        return refreshToken;
+    }
+
+    public static fromTokenOrThrow(token: string, error?: Error): BarkRefreshToken {
+
+        const refreshToken: BarkRefreshToken | null = BarkRefreshToken.fromTokenOrNull(token);
+
+        if (refreshToken === null) {
+            if (error) {
+                throw error;
+            }
+            throw panic.code(ERROR_CODE.INVALID_AUTHENTICATION_TOKEN_1, token);
+        }
+        return refreshToken;
     }
 
     private readonly _rawToken: string;
